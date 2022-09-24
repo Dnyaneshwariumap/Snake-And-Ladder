8db2214dd39183ae667a12f7261e948d41bfb3ce
Dnyaneshwari <dnyaneshwariumap994@gmail.com>
2022-09-24 16:28:37 +0530

    added:SnakeAndLadder

---
+++ Uc1SnakeAndLadder.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.day4;
+
+public class Uc1SnakeAndLadder {
+
+        static int position=0;
+        public static void main(String[] args) {
+
+        }
+
+    }
+
+
+
---
+++ Uc2SnakeAndLadder.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.day4;
+
+public class Uc2SnakeAndLadder {
+
+
+        static int position=0;
+        public static void main(String[] args) {
+
+            int diceNumber=(int) Math.floor(Math.random()*10)%6 + 1;
+            System.out.println("Dice Number is: " +diceNumber);
+        }
+
+    }
+
